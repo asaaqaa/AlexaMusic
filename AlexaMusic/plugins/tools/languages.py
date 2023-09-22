46e4a6833694e3c8263ec95080f49515aa9d9922
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-09-22 20:18:49 +0300

    Update languages.py

--- AlexaMusic/plugins/tools/languages.py
+++ AlexaMusic/plugins/tools/languages.py
@@ -30,7 +30,7 @@ def lanuages_keyboard(_):
             callback_data=f"languages:en",
         ),
         InlineKeyboardButton(
-            text="🇮🇳 हिन्दी 🇮🇳",
+            text="🇾🇪 عربي 🇾🇪",
             callback_data=f"languages:hi",
         ),
     )
